6a234eb16fbf34b1586c256b672773dab7d27ed6
lloydroskam1 <lloydroskam@gmail.com>
2019-01-12 15:31:49 +0100

    aanpassing 1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int Y = 35;
+        int X = 4;
+
+
+        
+        System.out.println(Y * X);
     }
 }
